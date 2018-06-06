34b5e84076181c81562603a9faa2fe982c0d781c
Rodrigo M. Mariano <rodmariano13@gmail.com>
2018-06-06 16:34:04 -0300

    Refactored the db_connection

--- models/util/__init__.py
+++ models/util/__init__.py
@@ -3,12 +3,11 @@
 
 from .common import *
 from .current_element import get_subquery_current_element_table
-from .user_group import get_subquery_user_group_table
-from .group import get_subquery_group_table
-from .project import get_subquery_project_table
-from .layer import get_subquery_layer_table, get_subquery_user_layer_table
+from .user import get_subquery_user_table
+from .curator import get_subquery_curator_table
+from .layer import get_subquery_layer_table
+from .user_layer import get_subquery_user_layer_table
 from .reference import get_subquery_reference_table
 from .keyword import get_subquery_keyword_table
 from .changeset import get_subquery_changeset_table
 from .notification import get_subquery_notification_table
-from .user import get_subquery_user_table
---
+++ models/util/curator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def get_subquery_curator_table(**kwargs):
+    # DEFAULT WHERE
+    # by default, get all results
+    conditions_of_where = []
+
+    # conditions of WHERE CLAUSE
+    if "user_id" in kwargs and kwargs["user_id"] is not None:
+        conditions_of_where.append("user_id = {0}".format(kwargs["user_id"]))
+
+    if "keyword_id" in kwargs and kwargs["keyword_id"] is not None:
+        conditions_of_where.append("keyword_id = '{0}'".format(kwargs["keyword_id"]))
+
+    if "region" in kwargs and kwargs["region"] is not None:
+        conditions_of_where.append("unaccent(LOWER(region)) LIKE '%' || unaccent(LOWER('{0}')) || '%'".format(kwargs["region"]))
+
+    # default get all features, without where clause
+    where_clause = ""
+
+    # if there is some conditions, put in where_clause
+    if conditions_of_where:
+        where_clause = "WHERE " + " AND ".join(conditions_of_where)
+
+    # default get all features
+    subquery_table = """
+        (
+            SELECT * FROM curator {0} ORDER BY user_id, keyword_id
+        ) AS curator
+    """.format(where_clause)
+
+    return subquery_table
