4800578c830a1a26c067372c9c4852dfd85eee76
Rodrigo M. Mariano <rodmariano13@gmail.com>
2018-06-07 20:07:44 -0300

    Admin user can delete user in layer

--- tests/test_api_user_layer.py
+++ tests/test_api_user_layer.py
@@ -16,7 +16,7 @@ class TestAPIUserLayer(TestCase):
         self.tester = UtilTester(self)
 
     # user_layer - get
-    
+
     def test_get_api_user_layer_return_all_users_in_layers(self):
         expected = {
             'features': [
@@ -192,6 +192,33 @@ class TestAPIUserLayer(TestCase):
             'type': 'UserLayer'
         }
         self.tester.api_user_layer_create(user_layer)
+
+        # get the id of layer to REMOVE it
+        user_id = user_layer["properties"]["user_id"]
+        layer_id = user_layer["properties"]["layer_id"]
+
+        # remove the user in layer
+        self.tester.api_user_layer_delete(user_id=user_id, layer_id=layer_id)
+
+        # it is not possible to find the layer that just deleted
+        self.tester.api_user_layer_error_404_not_found(user_id=user_id, layer_id=layer_id)
+
+        # DO LOGOUT AFTER THE TESTS
+        self.tester.auth_logout()
+
+    def test_api_user_layer_create_but_delete_with_admin(self):
+        # DO LOGIN
+        self.tester.auth_login("gabriel@admin.com", "gabriel")
+
+        # add a user in a layer
+        user_layer = {
+            'properties': {'is_the_creator': True, 'user_id': 1002, 'layer_id': 1003},
+            'type': 'UserLayer'
+        }
+        self.tester.api_user_layer_create(user_layer)
+
+        self.tester.auth_logout()
+        self.tester.auth_login("rodrigo@admin.com", "rodrigo")
 
         # get the id of layer to REMOVE it
         user_id = user_layer["properties"]["user_id"]
