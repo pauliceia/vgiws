c1273db425d4f0e3bba6d0e0b38d71863748a6fd
Rodrigo Mariano <rodmariano13@gmail.com>
2018-06-29 18:45:00 -0300

    Added invalid urls in tests and removed two files

--- tests/test_invalid_urls.py
+++ tests/test_invalid_urls.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
+
 from unittest import TestCase
-from requests import get
+from requests import get, post, put, delete
 
 
 class TestInvalidURLs(TestCase):
 
     def test_invalid_urls(self):
-        response = get('http://localhost:8888/api/nodex/create/')
+        response = get('http://localhost:8888/api/referencce')
 
         self.assertEqual(response.status_code, 404)
 
-        response = get('http://localhost:8888/api/element33/abx/')
+        response = post('http://localhost:8888/api/layyer/create/')
 
         self.assertEqual(response.status_code, 404)
 
-        response = get('http://localhost:8888/api/nodi/')
+        response = put('http://localhost:8888/apii/user')
 
         self.assertEqual(response.status_code, 404)
 
-        response = get('http://localhost:8888/areaa/nodi/')
+        response = delete('http://localhost:8888/api/keywoosd/382')
 
         self.assertEqual(response.status_code, 404)
 
-"""
+
 # It is not necessary to pyt the main() of unittest here,
 # because this file will be call by run_tests.py
